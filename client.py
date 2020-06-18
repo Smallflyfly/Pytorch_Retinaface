30e972e2488a705276f9994493f51f30987d84bd
方鹏飞 <fangpf5@chinaunicom.cn>
2020-06-18 15:13:49 +0800

    增加是否戴口罩识别

--- client.py
+++ client.py
@@ -6,6 +6,8 @@ import time
 
 import cv2
 import requests
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
 
 
 REQUEST_URL = "https://xiaomiqiu.ngrok2.xiaomiqiu.cn/upload"
@@ -34,12 +36,20 @@ if __name__ == '__main__':
         dets = predict(jpeg.tobytes())
         print('net forward time: {:.4f}'.format(time.time() - tic))
         for det in dets:
-            xmin, ymin, xmax, ymax, conf = det
+            xmin, ymin, xmax, ymax, conf, ismasked = det
             xmin = int(xmin * 4)
             ymin = int(ymin * 4)
             xmax = int(xmax * 4)
             ymax = int(ymax * 4)
             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 255, 0), 1)
+            image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            draw = ImageDraw.Draw(image)
+            fontStyle = ImageFont.truetype(
+                "font/FZY1JW.TTF", 20, encoding="utf-8"
+            )
+            draw.text((xmin, ymin-20), "佩戴口罩" if ismasked == 1 else "未佩戴口罩", (255, 0, 0), font=fontStyle)
+            frame = cv2.cvtColor(np.asarray(image), cv2.COLOR_BGR2RGB)
+
         cv2.imshow('im', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
